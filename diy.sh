ad23ab2277ddc87030dc198fc72334a43ddc5b8f
kasumiydz <92910045+kasumiydz@users.noreply.github.com>
2023-01-31 00:42:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,12 +7,3 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-#添加自定义组件
-src-git packages https://github.com/coolsnowwolf/packages
-src-git luci https://github.com/coolsnowwolf/luci
-src-git routing https://git.openwrt.org/feed/routing.git
-src-git telephony https://git.openwrt.org/feed/telephony.git
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
-src-git helloworld https://github.com/fw876/helloworld 
